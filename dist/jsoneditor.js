832ebe3cdb926d064e601170c12c203fbdb3b225
Brad Johnson <bradmeister@msn.com>
2015-08-11 15:57:12 +0800

    (Mike) fixed the numeric input form issue. (0 not being cleared on focus)

--- dist/jsoneditor.js
+++ dist/jsoneditor.js
@@ -2544,15 +2544,30 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
 });
 
 JSONEditor.defaults.editors.number = JSONEditor.defaults.editors.string.extend({
-  sanitize: function(value) {
-    return (value+"").replace(/[^0-9\.\-eE]/g,'');
-  },
-  getNumColumns: function() {
-    return 2;
-  },
-  getValue: function() {
-    return this.value*1;
-  }
+    sanitize: function (value) {
+        return (value + "").replace(/[^0-9\.\-eE]/g, '');
+    },
+    getNumColumns: function () {
+        return 2;
+    },
+    getValue: function () {
+        return this.value * 1;
+    },
+    build: function () {
+        this._super();
+        var self = this;
+
+        var eventName = 'focus'; // use onchange for select events, onclick for checkbox
+        this.input.addEventListener(eventName, function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            //When a numeric input is given focus, select all so new values don't prefix the redundant 0, default value
+            if (self.schema.format === "number") {
+                self.input.select();
+            }
+        });
+
+    }
 });
 
 JSONEditor.defaults.editors.integer = JSONEditor.defaults.editors.number.extend({
--- src/editors/number.js
+++ src/editors/number.js
@@ -1,11 +1,26 @@
 JSONEditor.defaults.editors.number = JSONEditor.defaults.editors.string.extend({
-  sanitize: function(value) {
-    return (value+"").replace(/[^0-9\.\-eE]/g,'');
-  },
-  getNumColumns: function() {
-    return 2;
-  },
-  getValue: function() {
-    return this.value*1;
-  }
+    sanitize: function (value) {
+        return (value + "").replace(/[^0-9\.\-eE]/g, '');
+    },
+    getNumColumns: function () {
+        return 2;
+    },
+    getValue: function () {
+        return this.value * 1;
+    },
+    build: function () {
+        this._super();
+        var self = this;
+
+        var eventName = 'focus'; // use onchange for select events, onclick for checkbox
+        this.input.addEventListener(eventName, function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            //When a numeric input is given focus, select all so new values don't prefix the redundant 0, default value
+            if (self.schema.format === "number") {
+                self.input.select();
+            }
+        });
+
+    }
 });
